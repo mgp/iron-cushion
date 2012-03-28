22478b26800d0ecef9cf9ffcd7af63497ba14b6c
Michael Parker <michael.g.parker@gmail.com>
2012-03-28 09:59:13 -0700

    Compute some statistics. Bleh.

--- couchdb-benchmark/src/co/adhoclabs/BenchmarkResults.java
+++ couchdb-benchmark/src/co/adhoclabs/BenchmarkResults.java
@@ -1,4 +1,7 @@
 package co.adhoclabs;
+
+import java.util.Arrays;
+
 /**
  * The results of the benchmark.
  * 
@@ -21,5 +24,58 @@ public class BenchmarkResults {
 		this.bytesReceived = bytesReceived;
 		this.requestsPerSecond = requestsPerSecond;
 		this.transferRate = transferRate;
+	}
+	
+	/**
+	 * Essential statistics about a data set.
+	 */
+	public static final class SampleStatistics {
+		public final double min;
+		public final double max;
+		public final double mean;
+		public final double median;
+		public final double deviation;
+		
+		public SampleStatistics(double min, double max,
+				double mean, double median, double deviation) {
+			this.min = min;
+			this.max = max;
+			this.mean = mean;
+			this.median = median;
+			this.deviation = deviation;
+		}
+		
+		private SampleStatistics statisticsForPopulation(long[] values) {
+			// Make a copy as a courtesy.
+			values = Arrays.copyOf(values, values.length);
+			// Find the minimum and maximum.
+			long min = values[0];
+			long max = values[values.length - 1];
+			// Find the median.
+			double median;
+			if ((values.length % 2) == 1) {
+				median = values[(values.length - 1) / 2];
+			} else {
+				int firstMedianIndex = values.length / 2;
+				int secondMedianIndex = firstMedianIndex - 1;
+				median = ((double) (values[firstMedianIndex] + values[secondMedianIndex])) / 2; 
+			}
+			// Compute the mean.
+			double sum = 0;
+			for (int i = 0; i < values.length; ++i) {
+				sum += values[i];
+			}
+			double mean = sum / values.length;
+			// Compute the standard deviation.
+			double numerator = 0;
+			for (int i = 0; i < values.length; ++i) {
+				double difference = values[i] - mean;
+				numerator += (difference * difference);
+			}
+			double variance = numerator / values.length;
+			double deviation = Math.sqrt(variance);
+			
+			return new SampleStatistics(min, max, mean, median, deviation);
+		}
 	}
 }
