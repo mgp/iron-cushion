a419df1485f0efc4effa60d4908d6fffb8dcf419
Michael Parker <michael.g.parker@gmail.com>
2012-03-29 22:46:52 -0700

    Remove unused imports.

--- iron-cushion/src/co/adhoclabs/ironcushion/DocumentSchema.java
+++ iron-cushion/src/co/adhoclabs/ironcushion/DocumentSchema.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
